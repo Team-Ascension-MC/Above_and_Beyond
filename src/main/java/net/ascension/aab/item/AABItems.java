60e5116cc49f0a94378ff52569b5919ca71866b2
ascens <128644895+ascenstohe4vn@users.noreply.github.com>
2025-05-08 21:59:57 -0400

    temp

---
+++ src/main/java/net/ascension/aab/item/AABItems.java
@@ -0,0 +1,18 @@
+package net.ascension.aab.item;
+
+import net.ascension.aab.AboveAndBeyond;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class AABItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(AboveAndBeyond.MOD_ID);
+
+    //Cloud Islands
+    public static final DeferredItem<Item> COMPACTED_WHITE_STUFF_BRICK = ITEMS.register("compacted_white_stuff_brick", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
