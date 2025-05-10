768f569931d5eee177665967834a169833ee7747
Ludovic Morin <ludovic@abstra208.com>
2025-05-10 01:24:04 -0400

    Update AABBlockLootTableProvider.java

--- src/main/java/net/ascension/aboveandbeyond/datagen/AABBlockLootTableProvider.java
+++ src/main/java/net/ascension/aboveandbeyond/datagen/AABBlockLootTableProvider.java
@@ -3,10 +3,19 @@ package net.ascension.aboveandbeyond.datagen;
 import net.ascension.aboveandbeyond.block.AABBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
 
@@ -17,9 +26,10 @@ public class AABBlockLootTableProvider extends BlockLootSubProvider {
 
     @Override
     protected void generate() {
-        dropSelf(AABBlocks.AEROSOIL.get());
-        dropSelf(AABBlocks.COARSE_AEROSOIL.get());
-        dropSelf(AABBlocks.ZEN_GRAVEL.get());
+        this.dropSelf(AABBlocks.PUFFGRASS_BLOCK.get());
+        this.dropSelf(AABBlocks.AEROSOIL.get());
+        this.dropSelf(AABBlocks.COARSE_AEROSOIL.get());
+        this.dropSelf(AABBlocks.ZEN_GRAVEL.get());
         this.dropSelf(AABBlocks.WELKIN_LOG.get());
         this.dropSelf(AABBlocks.WELKIN_WOOD.get());
         this.dropSelf(AABBlocks.STRIPPED_WELKIN_LOG.get());
@@ -35,7 +45,7 @@ public class AABBlockLootTableProvider extends BlockLootSubProvider {
                         .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))));
     }
     @Override
-    protected Iterable<Block> getKnownBlocks() {
+    protected @NotNull Iterable<Block> getKnownBlocks() {
         return AABBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
     }
 }
