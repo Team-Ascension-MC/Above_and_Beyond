8509cbb27edeca3aa46f4efa5cd2a1bb832515db
ascens <128644895+ascenstohe4vn@users.noreply.github.com>
2025-05-10 13:54:30 -0400

    disabled the loot table datagen until further notice

--- src/main/java/net/ascension/aboveandbeyond/datagen/AABBlockLootTableProvider.java
+++ src/main/java/net/ascension/aboveandbeyond/datagen/AABBlockLootTableProvider.java
@@ -1,3 +1,4 @@
+/* THIS IS TEMPORARILY DISABLED UNTIL WE FIX IT
 package net.ascension.aboveandbeyond.datagen;
 
 import net.ascension.aboveandbeyond.block.AABBlocks;
@@ -20,32 +21,34 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Set;
 
 public class AABBlockLootTableProvider extends BlockLootSubProvider {
-    protected AABBlockLootTableProvider(HolderLookup.Provider registries) {
-        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
-    }
-
-    @Override
-    protected void generate() {
-        this.dropSelf(AABBlocks.PUFFGRASS_BLOCK.get());
-        this.dropSelf(AABBlocks.AEROSOIL.get());
-        this.dropSelf(AABBlocks.COARSE_AEROSOIL.get());
-        this.dropSelf(AABBlocks.ZEN_GRAVEL.get());
-        this.dropSelf(AABBlocks.WELKIN_LOG.get());
-        this.dropSelf(AABBlocks.WELKIN_WOOD.get());
-        this.dropSelf(AABBlocks.STRIPPED_WELKIN_LOG.get());
-        this.dropSelf(AABBlocks.STRIPPED_WELKIN_WOOD.get());
-        this.dropSelf(AABBlocks.WELKIN_PLANKS.get());
-    }
-
-    protected LootTable.Builder createMultipleOreDrops(Block pBlock, Item item, float minDrops, float maxDrops) {
-        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
-        return this.createSilkTouchDispatchTable(pBlock,
-                this.applyExplosionDecay(pBlock, LootItem.lootTableItem(item)
-                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))
-                        .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))));
-    }
-    @Override
-    protected @NotNull Iterable<Block> getKnownBlocks() {
-        return AABBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
-    }
+protected AABBlockLootTableProvider(HolderLookup.Provider registries) {
+    super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
 }
+
+
+@Override
+protected void generate() {
+    this.dropSelf(AABBlocks.PUFFGRASS_BLOCK.get());
+    this.dropSelf(AABBlocks.AEROSOIL.get());
+    this.dropSelf(AABBlocks.COARSE_AEROSOIL.get());
+    this.dropSelf(AABBlocks.ZEN_GRAVEL.get());
+    this.dropSelf(AABBlocks.WELKIN_LOG.get());
+    this.dropSelf(AABBlocks.WELKIN_WOOD.get());
+    this.dropSelf(AABBlocks.STRIPPED_WELKIN_LOG.get());
+    this.dropSelf(AABBlocks.STRIPPED_WELKIN_WOOD.get());
+    this.dropSelf(AABBlocks.WELKIN_PLANKS.get());
+}
+
+protected LootTable.Builder createMultipleOreDrops(Block pBlock, Item item, float minDrops, float maxDrops) {
+    HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+    return this.createSilkTouchDispatchTable(pBlock,
+            this.applyExplosionDecay(pBlock, LootItem.lootTableItem(item)
+                    .apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))
+                    .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))));
+}
+@Override
+protected @NotNull Iterable<Block> getKnownBlocks() {
+    return AABBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+}
+}
+ */
