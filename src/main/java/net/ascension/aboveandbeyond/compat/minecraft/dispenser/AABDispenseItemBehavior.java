4d412d660644fbdaddf12e483f16751301effe7b
ascens <128644895+ascenstohe4vn@users.noreply.github.com>
2025-05-11 13:47:09 -0400

    added a dispenser behavior that doesnt even work

---
+++ src/main/java/net/ascension/aboveandbeyond/compat/minecraft/dispenser/AABDispenseItemBehavior.java
@@ -0,0 +1,32 @@
+package net.ascension.aboveandbeyond.compat.minecraft.dispenser;
+
+import net.ascension.aboveandbeyond.item.AABItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.dispenser.BlockSource;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.world.item.DispensibleContainerItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.DispenserBlock;
+import org.jetbrains.annotations.NotNull;
+
+public class AABDispenseItemBehavior {
+    public static void registerDispenseItemBehavior() {
+
+        DispenserBlock.registerBehavior(AABItems.CLOUD_BUCKET.get(), new DefaultDispenseItemBehavior() {
+            private final DefaultDispenseItemBehavior defaultDispenseItemBehavior = new DefaultDispenseItemBehavior();
+
+            @Override
+            protected @NotNull ItemStack execute(@NotNull BlockSource blockSource, @NotNull ItemStack item) {
+                DispensibleContainerItem dispensibleContainerItem = (DispensibleContainerItem) item.getItem();
+                BlockPos blockPos = blockSource.pos().relative(blockSource.state().getValue(DispenserBlock.FACING));
+                Level level = blockSource.level();
+                if (dispensibleContainerItem.emptyContents(null, level, blockPos, null)) {
+                    dispensibleContainerItem.checkExtraContent(null, level, item, blockPos);
+                    return new ItemStack(Items.BUCKET);
+                } else return this.defaultDispenseItemBehavior.dispense(blockSource, item);
+            }
+        });
+    }
+}
