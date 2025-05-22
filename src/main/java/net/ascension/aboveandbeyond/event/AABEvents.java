c7cd6343e11bfe995f22162c674cf07c033608ca
ascens <128644895+ascenstohe4vn@users.noreply.github.com>
2025-05-22 18:57:49 -0400

    made it a wandy t trade

--- src/main/java/net/ascension/aboveandbeyond/event/AABEvents.java
+++ src/main/java/net/ascension/aboveandbeyond/event/AABEvents.java
@@ -1,9 +1,8 @@
 package net.ascension.aboveandbeyond.event;
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.ascension.aboveandbeyond.AboveAndBeyond;
+import net.ascension.aboveandbeyond.block.AABBlocks;
 import net.ascension.aboveandbeyond.item.AABItems;
-import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -11,20 +10,23 @@ import net.minecraft.world.item.trading.ItemCost;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.village.VillagerTradesEvent;
+import net.neoforged.neoforge.event.village.WandererTradesEvent;
 
 import java.util.List;
 
 @EventBusSubscriber(modid = AboveAndBeyond.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
 public class AABEvents {
     @SubscribeEvent
-    public static void addCustomTrades(VillagerTradesEvent event) {
-        if (event.getType() == VillagerProfession.FARMER) {
-            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+    public static void addWanderingTrades(WandererTradesEvent event) {
+        List<VillagerTrades.ItemListing> genericTrades = event.getGenericTrades();
+ //maybe we could have some rare wandy t trade for a katana or something?       List<VillagerTrades.ItemListing> rareTrades = event.getRareTrades();
 
-            trades.get(2).add((entity, randomSource) -> new MerchantOffer(
-                    new ItemCost(AABItems.CLOUD_BERRY.get(), 4),
-                    new ItemStack(Items.EMERALD, 1), 12, 3, 0.05f));
-        }
+        genericTrades.add((entity, randomSource) -> new MerchantOffer(
+                new ItemCost(AABItems.CLOUD_BERRY, 4),
+                new ItemStack(Items.EMERALD, 1), 12, 3, 0.05f));
+
+        genericTrades.add((entity, randomSource) -> new MerchantOffer(
+                new ItemCost(Items.EMERALD, 5),
+                new ItemStack(AABBlocks.WELKIN_SAPLING.get(), 1), 8, 10, 0.05f));
     }
 }
