db3dcbbd0dc9dd738498252aabbbaecaf104fbcb
Ludovic Morin <ludovic@abstra208.com>
2025-07-14 08:21:37 -0400

    Update to the new model

--- src/main/java/net/ascension/aboveandbeyond/entity/client/KoiModel.java
+++ src/main/java/net/ascension/aboveandbeyond/entity/client/KoiModel.java
@@ -17,52 +17,40 @@ public class KoiModel<T extends KoiEntity> extends HierarchicalModel<T> {
     public static final ModelLayerLocation LAYER_LOCATION =
             new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(AboveAndBeyond.MOD_ID, "koi"), "main");
     private final ModelPart koi;
-    private final ModelPart frontBody;
+    private final ModelPart front;
 
     public KoiModel(ModelPart root) {
         this.koi = root.getChild("koi");
-        this.frontBody = this.koi.getChild("FrontBody");
+        this.front = this.koi.getChild("front");
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition koi = partdefinition.addOrReplaceChild("koi", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 19.0F, -5.0F, 0.0F, -0.0029F, 0.0F));
+        PartDefinition koi = partdefinition.addOrReplaceChild("koi", CubeListBuilder.create(), PartPose.offset(0.0F, 22.5F, 4.0F));
 
-        PartDefinition FrontBody = koi.addOrReplaceChild("FrontBody", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -4.0F, -8.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
-                .texOffs(14, 30).addBox(-2.0F, -1.0F, -9.5F, 4.0F, 4.0F, 2.0F, new CubeDeformation(0.0F))
-                .texOffs(16, 8).addBox(0.0F, -6.0F, -5.0F, 0.0F, 4.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0014F, -1.0F, 6.0F));
+        PartDefinition back = koi.addOrReplaceChild("back", CubeListBuilder.create().texOffs(18, 0).addBox(-1.5F, -2.0F, 0.0F, 3.0F, 3.0F, 2.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 7).addBox(0.0F, -3.0F, 1.5F, 0.0F, 5.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-        PartDefinition RightFrontFin = FrontBody.addOrReplaceChild("RightFrontFin", CubeListBuilder.create(), PartPose.offset(-4.0F, 4.0F, -2.0F));
+        PartDefinition middle = koi.addOrReplaceChild("middle", CubeListBuilder.create().texOffs(10, 5).addBox(0.0F, -2.0F, -3.0F, 0.0F, 2.0F, 7.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-2.5F, 0.0F, -5.0F, 5.0F, 4.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -2.5F, -3.0F));
 
-        PartDefinition cube_r1 = RightFrontFin.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(17, 25).addBox(-3.0F, 0.0F, -2.0F, 3.0F, 0.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.3927F));
+        PartDefinition front = koi.addOrReplaceChild("front", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -2.0F, -0.5F, 3.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.5F, 0.5F, -8.5F));
 
-        PartDefinition RightWhisker = FrontBody.addOrReplaceChild("RightWhisker", CubeListBuilder.create(), PartPose.offset(-2.0F, 1.0F, -8.0F));
+        PartDefinition cube_r1 = front.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(-1, 4).mirror().addBox(-2.0F, 0.0F, -0.5F, 2.0F, 0.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-3.0F, 0.0F, 0.0F, 0.0F, 0.0F, -1.0472F));
 
-        PartDefinition cube_r2 = RightWhisker.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(17, 20).addBox(-6.0F, 0.0F, -1.0F, 6.0F, 0.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.6109F));
+        PartDefinition cube_r2 = front.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(-1, 4).addBox(0.0F, 0.0F, -0.5F, 2.0F, 0.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0472F));
 
-        PartDefinition LeftWhisker = FrontBody.addOrReplaceChild("LeftWhisker", CubeListBuilder.create(), PartPose.offset(2.0F, 1.0F, -8.0F));
+        PartDefinition rightFin = koi.addOrReplaceChild("rightFin", CubeListBuilder.create(), PartPose.offset(-2.0F, 1.5F, -5.0F));
 
-        PartDefinition cube_r3 = LeftWhisker.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(19, 0).addBox(0.0F, 0.0F, -1.0F, 6.0F, 0.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.6109F));
+        PartDefinition cube_r3 = rightFin.addOrReplaceChild("cube_r3", CubeListBuilder.create().texOffs(24, 8).mirror().addBox(-2.0F, 0.0F, -1.0F, 2.0F, 0.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.5236F));
 
-        PartDefinition LeftFrontFin = FrontBody.addOrReplaceChild("LeftFrontFin", CubeListBuilder.create(), PartPose.offset(4.0F, 4.0F, -2.0F));
+        PartDefinition leftFin = koi.addOrReplaceChild("leftFin", CubeListBuilder.create(), PartPose.offset(2.0F, 1.5F, -5.0F));
 
-        PartDefinition cube_r4 = LeftFrontFin.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(23, 25).addBox(0.0F, 0.0F, -2.0F, 3.0F, 0.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.3927F));
+        PartDefinition cube_r4 = leftFin.addOrReplaceChild("cube_r4", CubeListBuilder.create().texOffs(24, 8).addBox(0.0F, 0.0F, -1.0F, 2.0F, 0.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.5236F));
 
-        PartDefinition BackBody = koi.addOrReplaceChild("BackBody", CubeListBuilder.create().texOffs(0, 16).addBox(-2.5F, -3.0F, 0.0F, 5.0F, 6.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0014F, -1.0F, 6.0F, 0.0F, 0.0264F, 0.0F));
-
-        PartDefinition BackLeftFin = BackBody.addOrReplaceChild("BackLeftFin", CubeListBuilder.create(), PartPose.offset(2.5F, 3.0F, 4.0F));
-
-        PartDefinition cube_r5 = BackLeftFin.addOrReplaceChild("cube_r5", CubeListBuilder.create().texOffs(0, 0).addBox(0.0F, 0.0F, -1.0F, 2.0F, 0.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.3927F));
-
-        PartDefinition Tail = BackBody.addOrReplaceChild("Tail", CubeListBuilder.create().texOffs(0, 21).addBox(0.0F, -6.0F, -2.0F, 0.0F, 11.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 6.0F, 0.0F, 0.0117F, 0.0F));
-
-        PartDefinition BackRightFin = BackBody.addOrReplaceChild("BackRightFin", CubeListBuilder.create(), PartPose.offset(-2.5F, 3.0F, 4.0F));
-
-        PartDefinition cube_r6 = BackRightFin.addOrReplaceChild("cube_r6", CubeListBuilder.create().texOffs(0, 4).addBox(-2.0F, 0.0F, -1.0F, 2.0F, 0.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, -0.3927F));
-
-        return LayerDefinition.create(meshdefinition, 64, 64);
+        return LayerDefinition.create(meshdefinition, 32, 32);
     }
 
     @Override
@@ -83,7 +71,7 @@ public class KoiModel<T extends KoiEntity> extends HierarchicalModel<T> {
     protected void applyHeadRotation(float headYaw, float headPitch) {
         headYaw = Mth.clamp(headYaw, -30f, 30f);
 
-        this.frontBody.yRot = headYaw * ((float)Math.PI / 180f);
+        this.front.yRot = headYaw * ((float)Math.PI / 180f);
     }
 
     @Override
